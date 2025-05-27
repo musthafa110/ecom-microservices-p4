c367e51aa58b56e7db028627917647fe2e5c9e04
musthafa110 <mmusthafakp786@gmail.com>
2025-05-28 00:51:08 +0530

    Update main.py

--- order-service/main.py
+++ order-service/main.py
@@ -4,6 +4,6 @@ from prometheus_fastapi_instrumentator import Instrumentator
 app = FastAPI()
 Instrumentator().instrument(app).expose(app)
 
-@app.get("/orders")
+@app.get("/")
 def get_orders():
     return [{"id": 1, "product": "Laptop"}, {"id": 2, "product": "Phone"}]
